f2e97cdde3f05418a9ecd4fc35d5ede8e9034d01
ramanpandey1 <ramanpandey1304@gmail.com>
2020-02-18 21:15:03 +0530

    final commmit 1

--- src/main/java/com/capgemini/greatoutdoors/util/ProductRepository.java
+++ src/main/java/com/capgemini/greatoutdoors/util/ProductRepository.java
@@ -8,6 +8,7 @@ import com.capgemini.greatoutdoors.dto.ProductDTO;
 
 public class ProductRepository {
 static public  HashMap<String, ProductDTO> productList=new HashMap<String , ProductDTO>(); 
+/************* Function To Create Repository for Product *************/
 static public void createProductList()
 {
 	productList.put("P101", new ProductDTO(1,10,"P101","i10","Red","dimension1","spec1","xyz",1000000));
@@ -16,9 +17,13 @@ static public void createProductList()
 	productList.put("P104", new ProductDTO(1,5,"P104","swift dezire","black","dimension3","spec1","xyz",500000));
 	productList.put("P105", new ProductDTO(1,10,"P105","creta","white","dimension1","spec2","xyz",800000));
 }
+
+/************** Function to get the list Of Product **************/
 public static HashMap<String, ProductDTO> getProductList() {
 	return productList;
 }
+
+/************** Function to set the list Of Product **************/
 public static void setProductList(HashMap<String, ProductDTO> productList) {
 	ProductRepository.productList = productList;
 }
